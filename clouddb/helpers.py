d8f81cfcb0ac186f8f7ff4f64d81b4e7051dd938
Andrew Regner <andrew@aregner.com>
2012-07-18 09:11:57 -0500

    helper method to sleep until the result of some operation is finished on the API.

--- clouddb/helpers.py
+++ clouddb/helpers.py
@@ -3,9 +3,11 @@ __author__ = "Andrew Regner <andrew@aregner.com>"
 
 """ This code is licensed under the MIT license.  See COPYING for more details. """
 
+import time
 import os
 
 from clouddb.models import *
+from clouddb import errors
 
 def build_from_list(self, klass, items):
     """
@@ -26,4 +28,35 @@ def get_from_id(self, klass, the_id):
         raise Exception("%s_id must be an integer" % kname)
     
     apiout = self.client.get('/%s/%d' % (kname, the_id))
-    return klass(parent = self, **apiout[kname])
+    return klass(**apiout[kname])
+
+def poll_for_result(self, path, status, timeout, first_poll, poll_interval):
+    """
+    """
+    # calculate when we are going to bail
+    drop_dead = time.time() + timeout
+    
+    # wait at least this long
+    time.sleep(first_poll)
+    
+    # check the status
+    apiresult = self.client.get(path)
+    
+    # these are the things we need to know
+    model = apiresult.keys()[0].title()
+    apiresult = apiresult.values()[0]
+    new_id = apiresult["id"]
+    
+    # check if it's "ready"
+    while apiresult['status'] != status:
+        if time.time() >= drop_dead:
+            # TODO : proper error
+            raise Exception("%(model)s timeout error.  New %(model)s id = %(id)s"
+                % {'model':model, 'id':new_id})
+            # or should this just return the instance?
+            #return Instance(**apiresult['instance'])
+        else:
+            time.sleep(poll_interval)
+        apiresult = self.client.get(path).values()[0]
+    
+    return apiresult
